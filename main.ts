907c2cad54fa5dad658ea8aeade5ea93c4b4230c
BenteLee <b.l.gubbels@gmail.com>
2020-04-30 12:51:51 +0200

    changed level 1 layout

--- main.ts
+++ main.ts
@@ -169,36 +169,66 @@ function levelStart () {
             [myTiles.tile0,sprites.castle.tileGrass1,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.castle.tileGrass3,sprites.castle.tileGrass2,sprites.builtin.forestTiles18,sprites.builtin.forestTiles17,sprites.builtin.forestTiles19,sprites.castle.tilePath8,sprites.castle.saplingOak,sprites.castle.saplingPine,sprites.castle.rock1,sprites.castle.rock0,sprites.castle.rock2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.castle.shrub,sprites.builtin.forestTiles0,sprites.builtin.forestTiles2,sprites.builtin.forestTiles3,sprites.builtin.forestTiles22,sprites.builtin.forestTiles21,sprites.builtin.forestTiles4],
             TileScale.Sixteen
         ))
+        tiles.setTilemap(tiles.createTilemap(
+            hex`2b001700111210191210111b141312141b10101c10101010101010101010101b14121313141b10101010101b14121314010b16010b100101130101010b0110010a010a010101140a01010a01010b011c010a1c010a010b0b0b12100103050401130a0114010a01010110010101010b0101120a010a010a01010a140a0a010101010b170b14110b0615070b1101011b0a01140a01100a01131214010a130101101011110101120101010a011c0b0b0b12100a06050901100a011001011b010110010a0101100101010b01100b0b110a011201011b1412141b1b1413100a0607010b1201011001011c01011001010101100a0a01010110010a1001011b0a01010a010a01010a1412100807101110010a100b0113010a121b14010a10010114010a100a011001011001010b0101010b01011b11110a1a11130a0101110101140a01010a010b0110010110010a110a0110010111010a1010111110101010120101010a01010b01110a011b01010101010101100101110a01110101100a01110a01100a01010b01011114010b01010101010110010110111110101b010a100a0a11010110010a10010a10010a1101010a010101101b01011c1b141301011c010a010b01010113010114010110010b140101120a0114010b101011111001011b1001011001010a010a120101010101010a01010101010b1001010a010a12010a0a010101010a011b0a0114100101100a0a010101131213010a14010a01010a01010110010a01010113010a0101010a0101011401011211010a10010113141213131201011b0101141b101013131c01011410131301011412131314010a13010b1310010110010b010a0101010b010110010b0a010101010b100a0101010a010b010a010101010101130101141b010a1b0a01010a010101010101110101010101010101110101010a0101010101010a01010b0112010114140101140101141c1b10101010111c111010101b14010111010b1c12141b10101111101b1413141c0b011b120101120a0a01010a010b01010101010a010a010101011001010101010a010a010a010101010a010101141301011301010a01010a01010a0a010101010a01010b0110010a010a010101010b01010b010a010a0a011413010b1313141b10101011101111101011111110140101140a01130a01131214101010111011101b1412131401010a0101010a01010101010a010a0101010a0101010101011201010b0101010101010a01010a010114140101010101010a0101010b01010101010a01010101010b010112010101010a0101010101010b010101101312141b10101111111010101c10101b1412131312141b101b141c141b1010111010101110101011101110`,
+            img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . 2 . . . . . 2 . . . . . . . 2 . . . . . . . . 2 . . 2 . . . . . . 2 
+2 . . . . . 2 . . 2 . . . . . 2 . . . . . . . 2 . . . . . . . . 2 . . . . . . . . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 . . 2 2 2 . . 2 . . 2 2 2 2 . . 2 . . . . . 2 . . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 . . . . 2 . . . . . 2 . . 2 . . 2 . . 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 . . . . 2 . . . . . 2 . . 2 . . 2 . . . . . . . . . 2 
+2 2 . . 2 2 2 . . 2 . . 2 . . 2 2 2 . . 2 . . 2 . . 2 . . 2 . . 2 . . . . . . . . . 2 
+2 2 . . 2 2 . . . 2 . . 2 . . . . . . . 2 . . 2 . . 2 . . 2 . . 2 . . 2 2 2 2 2 2 2 2 
+2 . . . . . . . . 2 . . 2 . . . . . . . 2 . . 2 . . 2 . . 2 . . 2 . . 2 . . . . . . 2 
+2 . . . . . . . . 2 . . 2 2 2 2 2 2 . . 2 . . 2 . . 2 . . 2 . . 2 . . 2 . . . . . . 2 
+2 . . 2 2 2 2 . . 2 . . . . . . . 2 . . 2 . . 2 . . 2 . . 2 . . 2 . . 2 2 2 2 2 . . 2 
+2 . . 2 . . . . . 2 . . . . . . . . . . . . . 2 . . . . . 2 . . . . . . . . . 2 . . 2 
+2 . . 2 . . . . . 2 2 2 . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . . . 2 . . 2 
+2 . . 2 . . 2 2 2 2 2 2 . . 2 . . 2 2 2 2 2 2 2 . . 2 2 2 2 . . 2 2 2 2 2 . . 2 . . 2 
+2 . . 2 . . . . . . . . . . 2 . . . . . . . . 2 . . . . . . . . . . . . . . . 2 . . 2 
+2 . . 2 . . . . . . . . . . 2 . . . . . . . . 2 . . . . . . . . . . . . . . . 2 . . 2 
+2 . . 2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 2 
+2 . . 2 . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . . . . 2 
+2 . . 2 . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . . . . 2 
+2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . 2 . . 2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,sprites.castle.tileGrass1,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath5,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.castle.tilePath7,sprites.castle.tilePath9,sprites.castle.tileGrass3,sprites.castle.tileGrass2,sprites.builtin.forestTiles18,sprites.builtin.forestTiles17,sprites.builtin.forestTiles19,sprites.castle.tilePath8,sprites.castle.saplingOak,sprites.castle.saplingPine,sprites.castle.rock1,sprites.castle.rock0,sprites.castle.rock2,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.castle.shrub,sprites.builtin.forestTiles0,sprites.builtin.forestTiles2,sprites.builtin.forestTiles3,sprites.builtin.forestTiles22,sprites.builtin.forestTiles21,sprites.builtin.forestTiles4,sprites.builtin.forestTiles1,sprites.castle.tileDarkGrass3],
+            TileScale.Sixteen
+        ))
         scene.setBackgroundColor(6)
         gotObject = 0
         flavorText = "Maar ik durf niet alleen door het bos "
         warningText = " en raak niet verdwaalt"
         tiles.placeOnRandomTile(playerSprite, myTiles.tile2)
         for (let value of tiles.getTilesByType(myTiles.tile2)) {
-            tiles.setTileAt(value, sprites.castle.tilePath5)
+            tiles.setTileAt(value, myTiles.tile5)
         }
         for (let value of tiles.getTilesByType(myTiles.tile1)) {
             questGiverSprite = sprites.create(img`
-. . . . . . . . . . . . . . . . 
-. . . f 4 4 f f f f . . . . . . 
-. . f 4 5 5 4 5 f b f f . . . . 
-. . f 5 5 5 5 4 e 3 3 b f . . . 
-. . f e 4 4 4 e 3 3 3 3 b f . . 
-. f 3 3 3 3 3 3 3 3 3 3 3 f . . 
-. f 3 3 e e 3 b e 3 3 3 3 f . . 
-. f 3 3 e e e f f 3 3 3 3 f . . 
-. . f e e e f b f b b b b f f . 
-. . . e 4 4 f 1 e b b b b b f . 
-. . . f 4 4 4 4 f b b b b b f . 
-. . . f d d d e 4 4 b b b f . . 
-. . . f d d d e 4 4 f f f . . . 
-. . f d d d b b e e b b f . . . 
-. . f b d 1 d 1 d d b f . . . . 
-. . . f f f b b f f f . . . . . 
+. . . . . f f 4 4 f f . . . . . 
+. . . . f 5 4 5 5 4 5 f . . . . 
+. . . f e 4 5 5 5 5 4 e f . . . 
+. . f b 3 e 4 4 4 4 e 3 b f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 f . . 
+. f 3 3 e b 3 e e 3 b e 3 3 f . 
+. f 3 3 f f e e e e f f 3 3 f . 
+. f b b f b f e e f b f b b f . 
+. f b b e 1 f 4 4 f 1 e b b f . 
+f f b b f 4 4 4 4 4 4 f b b f f 
+f b b f f f e e e e f f f b b f 
+. f e e f b d d d d b f e e f . 
+. . e 4 c d d d d d d c 4 e . . 
+. . e f b d b d b d b b f e . . 
+. . . f f 1 d 1 d 1 d f f . . . 
+. . . . . f f b b f f . . . . . 
 `, SpriteKind.questGiver)
             questGiverSprite.z = -1
             tiles.placeOnTile(questGiverSprite, value)
-            tiles.setTileAt(value, sprites.castle.tileGrass1)
+            tiles.setTileAt(value, sprites.castle.tilePath5)
         }
         for (let value of tiles.getTilesByType(myTiles.tile3)) {
             objectSprite = sprites.create(img`
@@ -232,7 +262,7 @@ function levelStart () {
         game.showLongText("" + PlayerName + " loopt verder er ziet verderop een jongen staan. hij lijkt te zoeken naar iets", DialogLayout.Full)
         ObjectGame = "spiegel"
         tiles.setTilemap(tiles.createTilemap(
-            hex`20001000111210101210111213131b1110141312131b10111b12111110131413101b111414010b0a010b100b010101011110101401010b01010a0101010a01010a010b1010010302040113011411110a01010a1101110113011011111301101412100111110b0616070b110111011410121301100a14011201140b011201010113011412100a08050901100a100a110a0101011001120a140a130101140a14010b011312100a011901151201100b110113010b11011301110113010b100112010b0112131210111a1011100111011001120101140a1101100a11010111011313010a01121111140111130b01120a120a11011312011b11100a1b1101010a0112011b0b1213010a010101010113181401100101010b010a01010a010b011001110a120114120113121411100113131401141110141314111011130b01011b0a10011401111b0b01010a010b0a01010a010b0101011301010b011401130111011101120b1b14011314101411111310141013011401110a101114130110011001010a1401101101140a010b010a0a010118110a130b1001010a01110a11010a010a1313101013011012111012111b11141011011301121411100110011313011314100b0b12120b010a010a010a010b01010a011201010a0101010a010114010101010b1712131411101112141110101111131314111012111b1110131214111b1210111113`,
+            hex`20001000111210191210111213131b1110141312131b10111b12111110131413101b111414010b16010b100b010101011110101401010b01010a0101010a01010a010b1010010305040113011411110a01010a1101110113011011111301101412100111110b0615070b110111011410121301100a14011201140b011201010113011412100a08050901100a100a110a0101011001120a140a130101140a14010b011312100a0119010b1201100b110113010b11011301110113010b100112010b0112131210111a1011100111011001120101140a1101100a11010111011313010a01121111140111130b01120a120a11011312011b11100a1b1101010a0112011b0b1213010a010101010113181401100101010b010a01010a010b011001110a120114120113121411100113131401141110141314111011130b01011b0a10011401111b0b01010a010b0a01010a010b0101011301010b011401130111011101120b1b14011314101411111310141013011401110a101114130110011001010a1401101101140a010b010a0a010118110a130b1001010a01110a11010a010a1313101013011012111012111b11141011011301121411100110011313011314100b0b12120b010a010a010a010b01010a011201010a0101010a010114010101010b1712131411101112141110101111131314111012111b1110131214111b1210111113`,
             img`
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 2 . . . . . 2 . . . . . 2 2 2 2 . . . . . . . . . . . . . . . 2 
@@ -260,30 +290,30 @@ function levelStart () {
         warningText = " en wees voorzichtig"
         tiles.placeOnRandomTile(playerSprite, myTiles.tile2)
         for (let value of tiles.getTilesByType(myTiles.tile2)) {
-            tiles.setTileAt(value, sprites.castle.tilePath5)
+            tiles.setTileAt(value, myTiles.tile5)
         }
         for (let value of tiles.getTilesByType(myTiles.tile1)) {
             questGiverSprite = sprites.create(img`
-. . . . f f f f f f . . . . . . 
-. . . f 2 f e e e e f f . . . . 
-. . f 2 2 2 f e e e e f f . . . 
-. . f e e e e f f e e e f . . . 
-. f e 2 2 2 2 e e f f f f . . . 
-. f 2 e f f f f 2 2 2 e f . . . 
-. f f f e e e f f f f f f f . . 
-. f e e 4 4 f b e 4 4 e f f . . 
-. . f e d d f 1 4 d 4 e e f . . 
-. . . f d d d d 4 e e e f . . . 
-. . . f e 4 4 4 e e f f . . . . 
-. . . f 2 2 2 e d d 4 . . . . . 
-. . . f 2 2 2 e d d e . . . . . 
-. . . f 5 5 4 f e e f . . . . . 
-. . . . f f f f f f . . . . . . 
-. . . . . . f f f . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
 `, SpriteKind.questGiver)
             questGiverSprite.z = -1
             tiles.placeOnTile(questGiverSprite, value)
-            tiles.setTileAt(value, sprites.castle.tileGrass1)
+            tiles.setTileAt(value, sprites.castle.tilePath5)
         }
         for (let value of tiles.getTilesByType(myTiles.tile3)) {
             objectSprite = sprites.create(img`
